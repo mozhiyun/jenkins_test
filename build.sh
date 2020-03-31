fb5d2a369edf0b48af8cb0b07389d4e483e12fe7
mozhiyun <zhangyi_19880124@126.com>
2020-03-31 18:49:46 +0800

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-IMAGE="jenkins_test:latest"​
+​
+IMAGE="jenkins_test:latest"
+​
 # 编译镜像
 docker build -t $IMAGE .
+​
 docker stop jenkins_test
+​
 docker run -d --name=jenkins_test --rm -p 8081:80 $IMAGE
+​
 # 清理
 docker rmi $IMAGE
