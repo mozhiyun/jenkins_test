a45e48229a497dcbb949cd90be39d40456d8d9d2
mozhiyun <zhangyi_19880124@126.com>
2020-03-31 18:35:50 +0800

    update

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 )
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Here is the home page. test2"))
+        w.Write([]byte("Here is the home page. test3"))
     })
     http.ListenAndServe(":80", nil)
 }
